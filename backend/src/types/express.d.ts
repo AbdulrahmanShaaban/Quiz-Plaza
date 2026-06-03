4da137183a327621dff86c5950ca13acefcf9093
AbdulrahmanShaaban <aboodxs3a@gmail.com>
2026-06-03 17:27:52 +0300

    meow

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -2,6 +2,8 @@ import { IUser } from "../models/User.js";
 
 declare global {
   namespace Express {
-    interface User extends IUser {}
+    interface Request {
+      user?: IUser;
+    }
   }
 }
